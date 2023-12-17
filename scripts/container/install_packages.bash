6524c101d95ad9d833d06d05b5e3aa35b5702db0
wav3m1nd3d <wav3m1nd3d@proton.me>
2023-12-17 04:39:29 +0100

    fix: Fragment RUN and Add ARG vars as script arguments

--- scripts/container/install_packages.bash
+++ scripts/container/install_packages.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
+rm -f /etc/apt/apt.conf.d/docker-clean
+echo 'Binary::apt::APT::Keep-Downloaded-Packages "true";' > /etc/apt/apt.conf.d/keep-cache
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 apt-get -qq update
 apt-get -qq --no-install-recommends upgrade
 apt-get -qq --no-install-recommends install $CONT_BASE_PKGS $CONT_PKGS
