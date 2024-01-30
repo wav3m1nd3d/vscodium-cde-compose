7fb538e8fa641f45e978cd9eb8eb5bf62292cb60
wav3m1nd3d <wav3m1nd3d@proton.me>
2024-01-30 20:30:04 +0100

    fix: Closes #13 and minor supervisord fixes

--- scripts/libs/lib_apply_conf.bash
+++ scripts/libs/lib_apply_conf.bash
@@ -5,9 +5,11 @@ apply_conf() {
 	if [[ -f "$TEMP_CONFIGS_DIR/${name}_${user}.conf" ]]; then
 		cat "$TEMP_CONFIGS_DIR/${name}_${user}.conf" | tee "$dest"
 	elif [[ -f "$TEMP_CONFIGS_DIR/${name}_e${user}.conf" ]]; then
-		while read line; do
+		_done=false
+		until $_done; do
+			read line || _done=true
 			eval "echo ${line}"
-		done <"$TEMP_CONFIGS_DIR/${name}_e${user}.conf" | tee "$dest"
+		done <"$TEMP_CONFIGS_DIR/${name}_e${user}.conf" > "$dest"
 	else
 		die 1 "Missing configuration files for $name in \"$TEMP_CONFIGS_DIR\" directory, should be named in format ${name}_$user.conf or ${name}_e$user.conf to evaluate variables"
 	fi
